403883b096a140844385d34efbcd20ef103ff176
0x663 <liushaokang98@gmail.com>
2024-03-08 14:57:45 +0800

    Initial Commit

---
+++ butt-lifting-assistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  butt-lifting-assistant
+//
+//  Created by 刘少康 on 3/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ butt-lifting-assistant/butt_lifting_assistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  butt_lifting_assistantApp.swift
+//  butt-lifting-assistant
+//
+//  Created by 刘少康 on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct butt_lifting_assistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
